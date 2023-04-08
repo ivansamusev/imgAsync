d4dbe59550cbe8873f62f7dc41734d38d3d3fbd2
ivan <ivansamusev044@gmail.com>
2023-04-08 13:57:09 +0300

    fetch

---
+++ src/index.js
@@ -0,0 +1,17 @@
+function photo(){
+setTimeout(() => {
+       fetch('https://jsonplaceholder.typicode.com/photos')
+  .then(response => response.json())
+  .then(data => {
+    console.log('slice :>> ', data.slice(0, 50));
+    data.slice(0, 50).forEach(item => {
+      const img  = document.createElement('img');
+        img.src= item.url
+      document.body.append(img);
+    });
+  })
+  .catch(error);   
+}, 1500);
+}
+
+photo()
